606f54b99a7bc93f207def895318af1784db2bb4
André Vítor Erin Strongren <andrevitorerin@gmail.com>
2020-05-21 05:30:44 -0300

    criação de novos exercícios

---
+++ ex007/condicao-simples.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log (`A velociodade do seu carro é: ${vel}km/h`)
+if (vel>60){
+    console.log (`Você ultrapassou o velocidade permitida. MULTADO!`)
+}
+console.log (`Dirija com cuidado`)
