de873496a41406813386a6dddab86474d2b81a43
André Vítor Erin Strongren <andrevitorerin@gmail.com>
2020-05-22 02:03:36 -0300

    Update for.js

--- ex009/for.js
+++ ex009/for.js
@@ -3,8 +3,7 @@
 var somatorio = 0;
 
 for (numAtual = 1; numAtual <= 10; numAtual ++) {
-  somatorio += numAtual;
-  numAtual++;
+  somatorio += numAtual;  
 }
 
 console.log (`O número atual é igual a ${somatorio}`)
